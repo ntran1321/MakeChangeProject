54eef130c955e9271abd1eaa0f535525c9617203
Nancy Tran <nancytran@SDMacBookPro3-2.local>
2017-04-07 15:17:15 -0600

    created readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+## Nancy's Make Change Project
+
+description about how you solved this problem
+how you code it
--- src/money/CashRegister.java
+++ src/money/CashRegister.java
@@ -1,8 +1,25 @@
 package money;
+import java.util.Scanner;
 
 public class CashRegister {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt user to enter purchase price
+		System.out.print("Purchase price: $");
+		double price = input.nextDouble();
+		
+		// Prompt user to enter tender amount
+		System.out.print("Tender amount: $");
+		double tender = input.nextDouble();
+		
+		// Too little or exact amount
+		if (price == tender) {
+			System.out.println("No change due.");
+		}
+		
+		
 		// Need a method to calculate change
 		// Need a method to handle user input
 		
