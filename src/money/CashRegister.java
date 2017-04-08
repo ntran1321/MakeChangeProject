d67d48bd7e9f6484309e4f23790eda37957ce436
Nancy Tran <nancytran@SDMacBookPro3-2.local>
2017-04-08 09:36:59 -0600

    reassign variables for coins

--- src/money/CashRegister.java
+++ src/money/CashRegister.java
@@ -1,89 +1,101 @@
 package money;
+
 import java.util.Scanner;
 
 public class CashRegister {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		// Prompt user to enter purchase price
 		System.out.print("Purchase price: $");
 		double price = input.nextDouble();
-		
+
 		// Prompt user to enter tender amount
 		System.out.print("Tender amount: $");
 		double tender = input.nextDouble();
 		
+		input.close();
+
 		// Too little or exact amount
 		if (price == tender) {
 			System.out.println("No change due.");
-		}
-		else {
+		} else if (price > tender) {
+			System.out.println("You owe me more money.");
+		} else {
 			double change = tender - price;
 			System.out.println("Change due: $" + change);
 			int bills;
-			bills = (int)change;
+			bills = (int) change;
 			System.out.println("Change in bills: $" + bills);
 			
 			double cents = change - bills;
-			System.out.println("cents: " + cents);	
-			
+			cents = Math.round(cents * 100);
+			//cents = cents / 100;
+			System.out.println("cents: " + cents);
+			//int coins = (int)cents * 100;
+			//System.out.println(coins);
+
 			giveBills(change, bills);
-			giveCoins(change, bills);
+			giveCoins(cents);
 		}
-	} //end main
-	
-	public static void giveBills(double change, int bills){
+	} // end main
+
+	public static void giveBills(double change, int bills) {
 		// ten dollar bills
-		int tens = bills / 10;
-		int fives = 0;
-		int ones = 0;
-		System.out.println(tens + " ten dollar bills");
-		
-		if (tens >= 1) {
-			bills = bills - 10 * tens;
-			fives = bills / 5;
-			System.out.println(fives + " five dollar bills");
+		while (bills != 0) {
+			int tens = 0;
+			int fives = 0;
+			int ones = 0;
+			if (bills >= 10) {
+				tens = bills / 10;
+				System.out.println(tens + " ten dollar bills");
+			}
+
+			if (tens >= 1) {
+				bills = bills - 10 * tens;
+				fives = bills / 5;
+				System.out.println(fives + " five dollar bills");
+			}
+
+			if (fives >= 1 || bills < 5) {
+				// bills = bills - (10 * tens + 5 * fives);
+				ones = bills % 5;
+				System.out.println(ones + " one dollar bills");
+			}
 		}
-		
-		if (fives >= 1) {
-			//bills = bills - (10 * tens + 5 * fives);
-			ones = bills % 5;
-			System.out.println(ones + " one dollar bills");
-		}
-		
-		
-//		if (bills >= 10 && bills % 5 != 0) {
-//			int tens = bills % 10;
-//			System.out.println("Give back " + tens + " ten dollar bill(s)");
-//		}
-//		if (bills < 5) {
-//			System.out.println("Give back " + bills + " one dollar bill(s)");
-//		}
-//		if (bills % 10 != 0 && bills >= 5) {
-//			int fives = bills / 5;
-//			System.out.println("Give back " + fives + " five dollar bill(s)");
-//		}
-//		
-		
-		
-			
-		// to find cents, keep decimals
-		// take price minus output of floor function
-		
-		// one dollar
-		// five dollar
-		// ten dollar
 	}
-	
-	public static void giveCoins(double change, int bills){
-		if (change < 1) {
-				
-			}		
-		
-				// penny
-				// nickel
-				// dime
-				// quarter
+
+	public static void giveCoins(double cents) {
+		int coins = (int)cents;
+		System.out.println("coins: " + coins);
+
+		while (coins != 0) {
+			int quarters = 0;
+			int dimes = 0;
+			int nickels = 0;
+			int pennies = 0;
+			
+			if (coins >= 25) {
+				quarters = coins / 25;
+				System.out.println(quarters + " quarters");
+			}
+
+			if (coins >= 10) {
+				coins = coins - (25 * quarters);
+				dimes = coins / 10;
+				System.out.println(dimes + " dimes");
+			}
+			if (coins >= 5 && coins % 25 != 0 && coins % 10 !=0) {
+				//coins = coins - 25 * quarters + 10 * dimes;
+				nickels = coins % 5;
+				System.out.println(nickels + " nickels");
+			}
+			if (nickels >= 1 || coins < 10) {
+				pennies = coins % 5;
+				System.out.println(pennies + " pennies");
+				break;
+			}
+		}
 	}
 }
